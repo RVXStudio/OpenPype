09ca2d665e44d5f4fbcc36e0ec7e8bec60c56949
Sveinbjorn J. Tryggvason <sveinbjorn@rvx.is>
2023-07-11 12:10:41 +0000

    disabling the assembly extraction

--- openpype/hosts/maya/plugins/publish/extract_assembly.py
+++ openpype/hosts/maya/plugins/publish/extract_assembly.py
@@ -18,6 +18,7 @@ class ExtractAssembly(publish.Extractor):
     label = "Extract Assembly"
     hosts = ["maya"]
     families = ["assembly"]
+    active = False
 
     def process(self, instance):
 
